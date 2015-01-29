f0f12f1c02ebcc8a1af021648eec39edea9df768
skootdiggity <smmiller.work@gmail.com>
2015-01-28 17:11:33 -0700

    COMPLETE

---
+++ mini-routing/js/timeDirective.js
@@ -0,0 +1,15 @@
+var app = angular.module('timeApp');
+
+app.directive('showTime', function($interval) {
+	return {
+		restrict: 'E',
+		template: '<div> The current time is {{time}} </div>',
+		link: function(scope, elem, attr) {
+			$interval(function() {
+				var currentTime = new Date();
+				scope.time= currentTime.toString();
+				console.log(currentTime)
+			}, 1000)
+		}
+	}
+});
